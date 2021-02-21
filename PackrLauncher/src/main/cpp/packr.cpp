4eed42b05262874bf03635072ed302ce80aed2ab
petoncle <petoncle@no.mail>
2021-02-21 13:23:23 +0100

    Add log

--- PackrLauncher/src/main/cpp/packr.cpp
+++ PackrLauncher/src/main/cpp/packr.cpp
@@ -202,7 +202,7 @@ static sajson::document readConfigurationFile(const string &fileName) {
     ifstream in(fileName.c_str(), std::ios::in | std::ios::binary);
     content = string((istreambuf_iterator<char>(in)), (istreambuf_iterator<char>()));
 #endif
-
+    cout << "readConfigurationFile content: " << content << endl;
     sajson::document json = sajson::parse(sajson::literal(content.c_str()));
     return json;
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@
 
 
 group = "com.badlogicgames.packr"
-version = "3.1.0.config35"
+version = "3.1.0.config36"
 
 ext["macOsMinimumVersion"] = "10.10"
 
